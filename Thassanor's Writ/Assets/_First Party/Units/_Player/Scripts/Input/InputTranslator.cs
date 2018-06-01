bdc50d5ae6d621ec3840be1de1ebe13b9ab549aa
Hayden Reeve <haydenreeve@hotmail.com>
2018-06-01 17:29:37 +0800

    Enum Framework for Character Controller.
    — Enum for Spellcasting, Idle, Paused in Character Controller.
    — Billboarder formatting and version update (forgot last time).

--- Thassanor's Writ/Assets/_First Party/Units/_Player/Scripts/Input/InputTranslator.cs
+++ Thassanor's Writ/Assets/_First Party/Units/_Player/Scripts/Input/InputTranslator.cs
@@ -1,7 +1,7 @@
 ﻿/* --------------------------------------------------------------------------------------------------------------------------------------------------------- //
    Author: 			Hayden Reeve
    File:			InputTranslator.cs
-   Version:			0.3.0
+   Version:			0.4.0
    Description: 	Translates the input provided by Tracker.cs Scripts into actual game functions that are located on the player object.
 // --------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
@@ -27,7 +27,9 @@ public class InputTranslator : MonoBehaviour {
 		Variables
 	// --------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
-
+    enum PlayerState {Idle, Spellcasting, Paused};
+    PlayerState _enPlayerState;
+    PlayerState _enLastState;
 
     /* --------------------------------------------------------------------------------------------------------------------------------------------------------- //
 		Instantation
@@ -38,20 +40,62 @@ public class InputTranslator : MonoBehaviour {
         _scControl = GetComponent<CharControls>();
         _scSpell = GetComponent<CharSpells>();
 
+        _enPlayerState = PlayerState.Idle;
+        _enLastState = _enPlayerState;
+
     }
 
     /* --------------------------------------------------------------------------------------------------------------------------------------------------------- //
 		Class Calls
 	// --------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
-	// A call that recieves inputs from the associated scripts. Any Inputs we're recieving are presented here and then translated into function-calls.
-	public void TranslateInput(RawDataInput rdi) {
+    // A call that recieves inputs from the associated scripts. Any Inputs we're recieving are presented here and then translated into function-calls.
+    public void TranslateInput(RawDataInput rdi) {
 
-        Debug.Log("--------------------");
-        Debug.Log($"Movement: Vertical [{rdi._aflAxes[0]}], Horizontal [{rdi._aflAxes[1]}]");
-		Debug.Log($"Buttons Pressed: Escape [{rdi._ablButtons[0]}], Enter [{rdi._ablButtons[1]}].");
-        
-        _scControl.TrajectoryChange(rdi._aflAxes);
+        // Some simple debugging that was originally useful.
+        // Debug.Log("--------------------");
+        // Debug.Log($"Movement: Vertical [{rdi._aflAxes[0]}], Horizontal [{rdi._aflAxes[1]}]");
+        // Debug.Log($"Buttons Pressed: Escape [{rdi._ablButtons[0]}], Enter [{rdi._ablButtons[1]}].");
+
+        // Pressing the ESC key should come first in our processes.
+        if (rdi._ablButtons[0]) {
+
+            switch (_enPlayerState) {
+
+				// If we're idle, pause the game.
+				case (PlayerState.Idle):
+                    _enLastState = _enPlayerState;
+                    _enPlayerState = PlayerState.Paused;
+					break;
+
+				// If we're casting a spell, stop casting.
+                case (PlayerState.Spellcasting):
+                    _enPlayerState = PlayerState.Idle;
+					// TODO: We want to do a little more than just shunt the PlayerState back to Idle. Call function here that represents the same command later.
+                    break;
+
+				// If we're paused, unpause the game.
+                case (PlayerState.Paused):
+                    _enPlayerState = _enLastState;
+                    break;
+
+            }
+
+        }
+
+		// Now, we should be checking each game state, as different states call for different actions.
+        switch (_enPlayerState) {
+
+			// If we're Idle, we want to process in-game commands.
+            case (PlayerState.Idle):
+                _scControl.TrajectoryChange(rdi._aflAxes);
+                break;
+
+			// If we're Spellcasting, we only really want to process final input as the keyboard is used for typing.
+            case (PlayerState.Spellcasting):
+                break;
+
+        }
 		
 	}
 

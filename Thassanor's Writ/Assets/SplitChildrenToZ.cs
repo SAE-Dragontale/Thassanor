25bf7f752eae789f6258ee23f5332e6e9de7855c
Hayden Reeve <haydenreeve@hotmail.com>
2018-06-19 14:43:44 +0800

    Sprite Re-Pass, Post processing Tweaks
    — Updated Acacia's Run Animation Pivot to match previous Idle Tweaks.
    — Changed PP Hue Shit from -10 to -5 to deviate from the original colour scheme less.
    — Included a 'Vertex Flat' plane with a large number of vertices to use for Shader Testing. (Temporary File)
    — Unpacked the Tree Sprite to use in a faux 3D effect.

---
+++ Thassanor's Writ/Assets/SplitChildrenToZ.cs
@@ -0,0 +1,25 @@
+﻿/* --------------------------------------------------------------------------------------------------------------------------------------------------------- //
+   Author: 			Hayden Reeve
+   File:			SplitChildrenToZ.cs
+   Version:			0.0.0
+   Description: 	A really simple placeholder script to manipulate sprite-children-components into a faux 3D effect.
+// --------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+using UnityEngine;
+
+public class SplitChildrenToZ : MonoBehaviour {
+
+	// Called before class calls or functions.
+	private void Start () {
+		
+		float i = 0f;
+		foreach (Transform tr in GetComponentInChildren<Transform>()) {
+			tr.position = new Vector3(tr.position.x, tr.position.y + (i*0.05f), tr.position.z - (i*0.1f));
+			i++;
+		}
+
+	}
+
+	/* ----------------------------------------------------------------------------- */
+	
+}

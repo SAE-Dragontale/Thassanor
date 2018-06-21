188f53bddbff5b1a8156c002c64eb2bc38691c1d
Travity-PC\Travity <1007590@student.sae.edu.au>
2018-06-22 00:29:54 +0800

    Options,Audio,Screen,Main Menu
    Added Mainmenu structure with Options, Audio and Screen

---
+++ Thassanor's Writ/Assets/_First Party/User Interface/Scripts/MainMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour {
+
+    [Header("Menu Containers")]
+    //Menu Containers
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public GameObject loreMenu;
+    public GameObject helpMenu;
+    public GameObject audioMenu;
+
+    [Header("Focus Buttons")]
+    //buttons to focus on (after menu change)
+    public GameObject hotkeysButton;
+    public GameObject playButton;
+    public GameObject audioSlider;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoreButton()
+    {
+
+    }
+
+    public void HelpButton()
+    {
+
+    }
+
+    public void OptionsButton()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(hotkeysButton);
+    }
+
+    public void QuitButton()
+    {
+
+    }
+
+    public void BackButtonOptions()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(playButton);
+    }
+
+    public void BackButtonAudio()
+    {
+        audioMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(hotkeysButton);
+    }
+
+    public void AudioButton()
+    {
+        optionsMenu.SetActive(false);
+        audioMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(audioSlider);
+    }
+}

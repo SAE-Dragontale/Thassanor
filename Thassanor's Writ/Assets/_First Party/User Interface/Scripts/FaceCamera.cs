688ff23845f051064a97c87014574987dbad6663
Philip Ong <33564159+travity@users.noreply.github.com>
2018-06-29 12:32:17 +0800

    Added Network Lobby to mainmenu scene with functionality + materials and scripts

---
+++ Thassanor's Writ/Assets/_First Party/User Interface/Scripts/FaceCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(Camera.main.transform.position);
+        this.transform.Rotate(new Vector3(0, 180, 0));
+	}
+}
---
+++ Thassanor's Writ/Assets/_First Party/User Interface/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+    [SyncVar]
+    public string pname = "player";
+
+    [SyncVar]
+    public Color playerColor = Color.white;
+
+    void OnGUI()
+    {
+        if (isLocalPlayer)
+        {
+            pname = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), pname);
+            if (GUI.Button(new Rect(130, Screen.height - 40, 80, 30), "Change"))
+            {
+                CmdChangeName(pname);
+            }
+        }
+
+    }
+
+    [Command]
+    public void CmdChangeName(string newName)
+    {
+        pname = newName;
+    }
+
+    // Use this for initialization
+    void Start () {
+        if (isLocalPlayer)
+        {
+            GetComponent<CarController>().enabled = true;
+            SmoothCameraFollow.target = this.transform;
+        }
+
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in renderers)
+            r.material.color = playerColor;
+
+        this.transform.position = new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+	}
+
+    private void Update()
+    {
+            this.GetComponentInChildren<TextMesh>().text = pname;
+    }
+}

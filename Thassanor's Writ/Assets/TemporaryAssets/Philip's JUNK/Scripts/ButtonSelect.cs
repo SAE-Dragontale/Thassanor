ae46414a8ee75de2f06d2487f4d162f68555621b
Philip Ong <33564159+travity@users.noreply.github.com>
2018-07-13 17:22:11 +0800

    Made a work around for Keybinding problems, fixed mousepointer stealing focus from keyboard

---
+++ Thassanor's Writ/Assets/TemporaryAssets/Philip's JUNK/Scripts/ButtonSelect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class ButtonSelect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+        private EventSystem eventSystem;
+        private GameObject lastSelectedObject;
+
+        // Use this for initialization
+        void Start()
+        {
+            eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+            lastSelectedObject = EventSystem.current.firstSelectedGameObject;
+    }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            eventSystem.SetSelectedGameObject(eventData.pointerEnter);
+            lastSelectedObject = eventSystem.currentSelectedGameObject;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            eventSystem.SetSelectedGameObject(lastSelectedObject);
+        }
+
+}
---
+++ Thassanor's Writ/Assets/TemporaryAssets/Philip's JUNK/Scripts/InitialiseButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InitialiseButton : MonoBehaviour
+{
+
+    GameObject lastSelect;
+
+    // Called at start of runtime
+    void Start()
+    {
+        lastSelect = EventSystem.current.firstSelectedGameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelect);
+        }
+        else
+        {
+            lastSelect = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+
+    public void CurrentMouseHover(GameObject focusButton)
+    {
+        lastSelect = focusButton;
+    }
+}

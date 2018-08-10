6ac28b792eff49de435fa78a64cdc02033f28e4b
Hayden Reeve <haydenreeve@hotmail.com>
2018-08-10 18:01:08 +0800

    GRass & Scene Changes [WIP]
    • Just setting up to recieve Justina's new sprite uploads.
    • Working through spawning Grass dynamically and creating a new tile prefab to use in our level generation.
    • Integrating Justina's background pixel art into the project. [WIP]

--- Thassanor's Writ/Assets/_First Party/Setting/Terrain/Scripts/Environment/PopulateGrass.cs
+++ Thassanor's Writ/Assets/_First Party/Setting/Terrain/Scripts/Environment/PopulateGrass.cs
@@ -1,49 +1,58 @@
 ﻿/* --------------------------------------------------------------------------------------------------------------------------------------------------------- //
    Author: 			Hayden Reeve
-   File:			Billboarder.cs
-   Version:			0.2.1
-   Description: 	A simple script to automatically rotate sprites to face the player camera.
+   File:			PopulateGrass.cs
+   Version:			0.0.0
+   Description: 	
 // --------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Billboarder : MonoBehaviour {
+public class PopulateGrass : MonoBehaviour {
 
 	/* --------------------------------------------------------------------------------------------------------------------------------------------------------- //
 		References
 	// --------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
-	[SerializeField] protected Transform _trCamera;
+	private MeshRenderer _mr;	// We need our Mesh Renderer to determine the area of the space we have to work with when spawning grass.
 
 	/* --------------------------------------------------------------------------------------------------------------------------------------------------------- //
-		Initialisation
+		Variables
 	// --------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
-	// Called before Start()
-	protected void Awake() {
+	[SerializeField] private GameObject[] _agoGrass;	// The various grass prefabs that we can choose to spawn.
+	[SerializeField] private int _itAmountOfGrass;		// How many of said prefabs do we want to spawn?
 
-		//_trCamera = _trCamera?.GetComponent<Transform>() ?? Camera.main.GetComponent<Transform>();
-		_trCamera = Camera.main.GetComponent<Transform>();
-
-	}
 
 	/* --------------------------------------------------------------------------------------------------------------------------------------------------------- //
-		Class Runtime
+		Instantation
 	// --------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
-	// Update is called once per frame
-	protected void FixedUpdate () {
+	// Called before Start().
+	private void Awake() {
 
-		Billboard(transform);
-		
-	}
-
-	// Called whenevger we need to billboard something.
-	protected void Billboard(Transform transformToBillboard) {
-
-		// We're simply asking the sprite to look towards the camera.
-		transformToBillboard.LookAt(transform.position + _trCamera.transform.rotation * Vector3.forward, _trCamera.transform.rotation * Vector3.up);
+		_mr = GetComponent<MeshRenderer>();
 
 	}
 
+	// Called before class calls or functions.
+	private void Start() {
+
+		Vector3 v3Size = _mr.bounds.extents;
+
+		for (int it = _itAmountOfGrass; _itAmountOfGrass > 0; _itAmountOfGrass--) {
+
+			// Find our our position within the bounds of our Mesh Renderer (surface) and instantiate some grass!
+			Vector3 v3Place = _mr.bounds.center + new Vector3(Random.Range(-v3Size.x/2,v3Size.x/2), Random.Range(-v3Size.y / 2, v3Size.y / 2), 0f);
+			Instantiate(_agoGrass[Random.Range(0, _agoGrass.Length - 1)], v3Place, Quaternion.identity);
+
+		}
+
+		// We no longer require this script. Delete the evidence, Mr Burns.
+		Destroy(this);
+
+	}
+
+	/* ----------------------------------------------------------------------------- */
+	
 }

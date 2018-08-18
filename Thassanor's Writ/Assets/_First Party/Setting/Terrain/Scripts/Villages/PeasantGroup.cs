7ce1b1c29d1944c47b4f3cdcc7ef64fd7d9bb55d
DESKTOP-FJBDE7D\Eric's PC <Jacob.cox@outlook.com>
2018-08-18 15:02:46 +0800

    Peasant Movement
    • Peasant Movement (They still cluster around world 0,0,0 from thhe looks of it)
    • Debugged Vector for movement to see if passing through
    • Removed old assets
    • Attempted to make proppeller spin

---
+++ Thassanor's Writ/Assets/_First Party/Setting/Terrain/Scripts/Villages/PeasantGroup.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PeasantGroup : UnitGroup {
+
+    Vector3 _patrolDir;
+    float _timer;
+
+
+
+    protected override void BehaviourLoopPassive()
+    {
+        _timer = _timer + Time.deltaTime;
+        if (_timer >= 3f)
+        {
+            for (int i = 0; i < _everyUnit.Length; i++)
+            {
+                //chooses a random patrol direction out of 4 directions
+                float patrolDirX = Random.Range(-1f, 1f);
+                float patrolDirZ = Random.Range(-1f, 1f);
+
+                _patrolDir = new Vector3(patrolDirX, 0, patrolDirZ);
+                Debug.Log("Move Unit " + i + " to" + _patrolDir);
+                MoveUnit(i, _patrolDir);
+
+
+            }
+            _timer = 0f;
+        }
+        
+    }
+
+
+
+    //_unitStyle;           // The type of unit that we contain.
+    //_unitTemplate;        // The basic unit template.
+    //_everyUnit;           // The units within the group.
+
+
+    //  code to get units to move 
+    //set health
+    //BehaviourLoopPassive
+    //NeedToUpdatePosition
+    //PositionVariance
+    //MoveUnit
+
+
+}
